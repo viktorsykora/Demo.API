5b3869d97ccd8fac5b4b4f1a702a8a8a1131b551
Viktor Sýkora <viktor.sykora@hotmail.com>
2025-06-15 17:00:05 +0200

    Added simple test for GetAveragePerCountryQueryHandler

--- Demo.Tests/GetAveragePerCountryQueryHandlerTest.cs
+++ Demo.Tests/GetAveragePerCountryQueryHandlerTest.cs
@@ -1,10 +1,41 @@
-﻿namespace Demo.Tests
+﻿using Demo.Application.Abstractions;
+using Demo.Application.Features.GrossWrittenPremium.Queries;
+using Demo.Domain;
+using Demo.Shared.Enums;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MockQueryable.Moq;
+using Moq;
+
+namespace Demo.Tests
 {
+    [TestClass]
     public class GetAveragePerCountryQueryHandlerTest
     {
-        public void Test_GetAveragePerCountryQueryHandler()
+        [TestMethod]
+        public async Task GetAveragePerCountryQueryHandler_Must_Return_Avg()
         {
-           
+            List<GrossWrittenPremium> testData = new List<GrossWrittenPremium>
+            {
+                new GrossWrittenPremium { Year = 2008, Amount = 100m, LineOfBusiness = LineOfBusiness.Property, Country = Country.Ae },
+                new GrossWrittenPremium { Year = 2009, Amount = 200m, LineOfBusiness = LineOfBusiness.Property, Country = Country.Ae }
+            };
+
+            var mockSet = testData.AsQueryable().BuildMockDbSet();
+            var mockContext = new Mock<IApplicationDbContext>();
+            mockContext.Setup(x => x.GrossWrittenPremiums).Returns(mockSet.Object);
+
+            var handler = new GetAveragePerCountryQueryHandler(mockContext.Object);
+            var query = new GetAveragePerCountryQuery
+            {
+                Country = Country.Ae,
+                LinesOfBusiness = [ LineOfBusiness.Property ],
+                YearSince = 2008,
+                YearUntil = 2009
+            };
+
+            var result = await handler.Handle(query, CancellationToken.None);
+            
+            Assert.AreEqual(150, result[LineOfBusiness.Property]);
         }
     }
 }
